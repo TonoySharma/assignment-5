0beaf50b22af2c00360acf9f0567f7b893e0e353
Your Name <you@example.com>
2026-03-07 00:32:45 +0600

    first commit

---
+++ script/sign-in.js
@@ -0,0 +1,22 @@
+console.log("sign in functionality comming");
+
+document.getElementById("sign-in-btn").addEventListener("click" , function(){
+    // console.log("sign-in-btn button click");
+// 1.get username input
+    const inputUsername = document.getElementById("input-username");
+    const userName = inputUsername .value;
+    console.log(userName);
+// 2.get password input
+    const inputPassword = document.getElementById("input-password");
+    const password = inputPassword.value;
+    console.log(password);
+// 3.match username & pass
+    if(userName=="admin" && password=="admin123"){
+        // alert("Sign In Success");
+    }else{
+        alert("Sign In Failed");
+        return;
+    }
+
+
+});
